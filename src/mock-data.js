7197e86acac56021e36180ad5a0fa93f7a4941f9
ilukin89 <67657429+ilukin89@users.noreply.github.com>
2021-11-24 18:20:01 +0100

    unit testing

---
+++ src/mock-data.js
@@ -0,0 +1,114 @@
+const mockData = [
+  {
+    "kind": "calendar#event",
+    "etag": "\"3181161784712000\"",
+    "id": "4eahs9ghkhrvkld72hogu9ph3e_20200519T140000Z",
+    "status": "confirmed",
+    "htmlLink": "https://www.google.com/calendar/event?eid=NGVhaHM5Z2hraHJ2a2xkNzJob2d1OXBoM2VfMjAyMDA1MTlUMTQwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+    "created": "2020-05-19T19:17:46.000Z",
+    "updated": "2020-05-27T12:01:32.356Z",
+    "summary": "Learn JavaScript",
+    "description": "Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.",
+    "location": "London, UK",
+    "creator": {
+     "email": "fullstackwebdev@careerfoundry.com",
+     "self": true
+    },
+    "organizer": {
+     "email": "fullstackwebdev@careerfoundry.com",
+     "self": true
+    },
+    "start": {
+     "dateTime": "2020-05-19T16:00:00+02:00",
+     "timeZone": "Europe/Berlin"
+    },
+    "end": {
+     "dateTime": "2020-05-19T17:00:00+02:00",
+     "timeZone": "Europe/Berlin"
+    },
+    "recurringEventId": "4eahs9ghkhrvkld72hogu9ph3e",
+    "originalStartTime": {
+     "dateTime": "2020-05-19T16:00:00+02:00",
+     "timeZone": "Europe/Berlin"
+    },
+    "iCalUID": "4eahs9ghkhrvkld72hogu9ph3e@google.com",
+    "sequence": 0,
+    "reminders": {
+     "useDefault": true
+    },
+    "eventType": "default"
+   },
+
+   {
+    "kind": "calendar#event",
+    "etag": "\"3187222416180000\"",
+    "id": "6co0ojl0l0j0rbh3g56vlc2s9i_20200812T010000Z",
+    "status": "confirmed",
+    "htmlLink": "https://www.google.com/calendar/event?eid=NmNvMG9qbDBsMGowcmJoM2c1NnZsYzJzOWlfMjAyMDA4MTJUMDEwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+    "created": "2020-07-01T13:44:15.000Z",
+    "updated": "2020-07-01T13:46:48.090Z",
+    "summary": "React California",
+    "description": "React is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nReact California is a non-profit organization offering free training sessions to React enthusiasts every Wednesday and Friday.\n\nJoin us in our free React training sessions and give your career a new direction.",
+    "location": "California, USA",
+    "creator": {
+     "email": "fullstackwebdev@careerfoundry.com",
+     "self": true
+    },
+    "organizer": {
+     "email": "fullstackwebdev@careerfoundry.com",
+     "self": true
+    },
+    "start": {
+     "dateTime": "2020-08-12T03:00:00+02:00",
+     "timeZone": "Europe/Berlin"
+    },
+    "end": {
+     "dateTime": "2020-08-12T04:00:00+02:00",
+     "timeZone": "Europe/Berlin"
+    },
+    "recurringEventId": "6co0ojl0l0j0rbh3g56vlc2s9i",
+    "originalStartTime": {
+     "dateTime": "2020-08-12T03:00:00+02:00",
+     "timeZone": "Europe/Berlin"
+    },
+    "iCalUID": "6co0ojl0l0j0rbh3g56vlc2s9i@google.com",
+    "sequence": 1,
+    "hangoutLink": "https://meet.google.com/imt-kabh-cib",
+    "conferenceData": {
+     "entryPoints": [
+      {
+       "entryPointType": "video",
+       "uri": "https://meet.google.com/imt-kabh-cib",
+       "label": "meet.google.com/imt-kabh-cib"
+      },
+      {
+       "entryPointType": "more",
+       "uri": "https://tel.meet/imt-kabh-cib?pin=8711708481601",
+       "pin": "8711708481601"
+      },
+      {
+       "regionCode": "DE",
+       "entryPointType": "phone",
+       "uri": "tel:+49-40-8081618688",
+       "label": "+49 40 8081618688",
+       "pin": "752695860"
+      }
+     ],
+     "conferenceSolution": {
+      "key": {
+       "type": "hangoutsMeet"
+      },
+      "name": "Google Meet",
+      "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+     },
+     "conferenceId": "imt-kabh-cib",
+     "signature": "AGirE/In05cchoOvbYk33gmlrnrg"
+    },
+    "reminders": {
+     "useDefault": true
+    },
+    "eventType": "default"
+   }
+
+];
+export { mockData };
