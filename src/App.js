e7c97f37d43b38fa033c26e5f319633e4e893f4d
ilukin89 <67657429+ilukin89@users.noreply.github.com>
2021-12-15 13:46:19 +0100

    privacy.html added, token updated

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import './nprogress.css';
+import WelcomeScreen from './WelcomeScreen';
 
 
 class App extends Component {
@@ -14,56 +15,75 @@ class App extends Component {
     locations: [],
     numberOfEvents: 32,
     currentLocation: 'all',
+    showWelcomeScreen: undefined
   }
 
-// Function for updating the events state when a city is selected
+  // Function for updating the events state when a city is selected
 
   updateEvents = (location) => {
-  getEvents().then((events) => {
-    const locationEvents = (location === 'all') ?
-      events :
-      events.filter((event) => event.location === location);
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
       const shownEvents = locationEvents.slice(0, this.state.numberOfEvents);
-    this.setState({
-      events: shownEvents,
-      currentLocation: location
+      this.setState({
+        events: shownEvents,
+        currentLocation: location
+      });
     });
-  });
-}
+  }
 
   // Function to update the number of events displaying on the page when the user inputs a new number
 
   updateNumberofEvents = (newNumberOfEvents) => {
-  this.setState({
-    numberOfEvents: newNumberOfEvents
-  });
-  this.updateEvents(this.state.currentLocation);
-}
+    this.setState({
+      numberOfEvents: newNumberOfEvents
+    });
+    this.updateEvents(this.state.currentLocation);
+  }
 
-componentDidMount() {
-  this.mounted = true;
-  getEvents().then((events) => {
-    const { numberOfEvents} = this.state;
-    if (this.mounted) {
-      const locationEvents = events.slice(0, numberOfEvents);
-      this.setState({ 
-        events: locationEvents, 
-        locations: extractLocations(events) });
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
     }
-  });
-}
+    // getEvents().then((events) => {
+    //   const { numberOfEvents } = this.state;
+    //   if (this.mounted) {
+    //     const locationEvents = events.slice(0, numberOfEvents);
+    //     this.setState({
+    //       events: locationEvents,
+    //       locations: extractLocations(events)
+    //     });
+    //   }
+    // });
+  }
 
-componentWillUnmount(){
-  this.mounted = false;
-}
+  componentWillUnmount() {
+    this.mounted = false;
+  }
 
   render() {
-    const {numberOfEvents, locations, events} = this.state;
+    if (this.state.showWelcomeScreen === undefined) return <div
+      className="App" />
+    const { numberOfEvents, locations, events } = this.state;
     return (
       <div className="App">
         <CitySearch locations={locations} updateEvents={this.updateEvents} />
-        <EventList events={events}/>
+        <EventList events={events} />
         <NumberOfEvents updateNumberofEvents={this.updateNumberofEvents} numberOfEvents={numberOfEvents} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
